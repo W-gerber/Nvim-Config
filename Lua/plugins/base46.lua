231a200460322a51c42e8c1354ef171d287c749b
Willem Gerber <wgerber04@icloud.com>
2026-08-01 10:28:29 +0200

    Delete Lua directory

--- Lua/plugins/base46.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "NvChad/base46",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- base46 uses this cache path for compiled highlights
-    vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
-  end,
-}
--- Lua/plugins/smear_cursor.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  event = "VeryLazy",
-  opts = {
-    smear_distance = 15,
-    smear_blend = 0.65,
-    elastic = true,
-  },
-}
